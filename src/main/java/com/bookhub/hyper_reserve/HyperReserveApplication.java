ff4ff76a46dc7e77b34c69cbf2cb4cb4e0fd4ee8
BookHub <alex204301@naver.com>
2026-03-03 21:46:33 +0900

    로컬 개발 설정 Commit

--- src/main/java/com/bookhub/hyper_reserve/HyperReserveApplication.java
+++ src/main/java/com/bookhub/hyper_reserve/HyperReserveApplication.java
@@ -1,4 +1,11 @@
 package com.bookhub.hyper_reserve;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HyperReserveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HyperReserveApplication.class, args);
+    }
 }
